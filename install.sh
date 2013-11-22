15e900b77f522432fa086a0697e0cde6a0cfa9da
jokerfeng2010@gmail.com <jokerfeng2010@gmail.com>
2013-11-22 01:15:54 +0000

    fengjie

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
 git clone https://github.com/cakephp/debug_kit app/Plugin/DebugKit
-mkdir app/tmp/cache/
-mkdir app/tmp/logs/
-mkdir app/tmp/sessions/
-mkdir app/tmp/tests/
-chmod 777 app/tmp/* -R
+cp tmp app/tmp -R
+chmod 777 app/tmp -R
